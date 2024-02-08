59d56d27e07f2dbd5180c0a126461356267f0945
Javier Castro <javier_castro@fragua.com.mx>
2024-02-08 16:50:08 -0600

    Add css propperties

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Tailwind CSS. Se debe agregar al array cada una de las integraciones que se deseen utilizar
+  integrations: [tailwind()]
+});
